9f3ddc7a05e24cd7b415a7514762a767a6c04f0e
Ravjodh Heer <ravjodhheer@Ravjodhs-MacBook-Pro.local>
2022-11-03 14:20:29 -0700

    profile page and rentals UI all merged and uploaded

--- Roomy/src/components/Profile.jsx
+++ Roomy/src/components/Profile.jsx
@@ -1,112 +1,306 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
-import { useParams, Redirect } from "react-router-dom";
-import { getUserAuth } from "../action";
 import styled from "styled-components";
 import db, { auth, provider, storage } from "../firebase";
-import { signOutAPI } from "../action";
-import Sidebar from "./Sidebar";
-import Header from "./Header";
+import Sidebar from "./Sidebar"
+import Header from "./Header"
 
-const Head = styled.div`
-    .top {                                           //This is all the top of the header stuff where the purple banner is
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        margin-left: auto;
-        margin-right: auto;
-        padding: 15px;
-        color: white;
-        width: 100%;
-        background: linear-gradient(0deg, #F2F3F5 25%, #A943D3 0%);
+const Container = styled.div`
+	grid-area: center;
+    align-items: center;
+    background-size: cover;
+    padding: 70px 20px 0px 255px;
 
-    }
 
+
+	input {
+		background-color: #ffffff;
+		border-radius: 20px;
+
+	}
 `;
 
+const ArtCard = styled.div`
+    position: relative;
+	text-align: center;
+	overflow: hidden;
+	margin-bottom: 8px;
+	border-radius: 5px;
+	background-color: #fff;
+	transition: box-shadow 83ms;
+	position: relative;
+	border: none;
+	box-shadow: 0 0 3px #999, 0 0 0 rgb(0 0 0 / 20%);
+`;
+
+const UserInfo = styled.div`
+	border-bottom: 1px solid rgba(0, 0, 0, 0.15);
+	padding: 12px 12px 16px;
+	word-wrap: break-word;
+	word-break: break-word;
+`;
+
+const CardBackground = styled.div`
+	background: url("/images/card-bg.png");
+	background-position: center;
+	background-size: 1200px;
+	height: 82px;
+	margin: -12px -12px 0;
+`;
 
 const Photo = styled.div`
-    img {
-        margin-top: 100px;
-        width: 350px;  
-        outline: solid;
-        border-radius: 175px; 
-        box-shadow: 0px 0px 20px #777;
-    }
-`;
-const Grid = styled.div`
-    display: grid;
-    justify-content: center;
-    grid-template-rows: repeat(4, 100px);
-    grid-gap: 20px;
-
-`;
-const Div = styled.div`
-    display: flex;
-    border: 1px solid #ccc;
-    justify-content: center;
-    border-radius: 20px;
-    //border-color: gray
-    height: 200px;
-    width: 1200px;
-    text-align: center;
-
-    h2 {
-        font-size: 40px;
-        font: Lucida Console;
-        color: black;
-    }
-
-    h1 {
-        margin-right: 900px
-    }
-
-    span {
-        font-size: 45px;
-    }
+	box-shadow: none;
+	background: url(${props => props.photoUrl});
+	width: 100px;
+	height: 100px;
+	box-sizing: border-box;
+	background-clip: content-box;
+	background-color: #fff;
+	background-position: center;
+	background-size: 100%;
+	background-repeat: no-repeat;
+	border: 2px solid white;
+	margin: -38px auto 12px;
+	border-radius: 50%;
 `;
 
+const Link = styled.div`
+	font-size: 16px;
+	line-height: 1.5;
+	color: rgba(0, 0, 0, 0.9);
+	font-weight: 600;
+`;
 
-function Profile(props){
-    let user = props.user ? props.user : null;
-    let photoUrl = props.user ? props.user.photoUrl : null;
-    return (<div>
-        <Header />
+const AddPhotoText = styled.div`
+	color: #8f2bb8;
+	margin-top: 4px;
+	font-size: 12px;
+	line-height: 1.33;
+	font-weight: 400;
+`;
+
+const Widget = styled.div`
+	border-bottom: 1px solid rgba(0, 0, 0, 0.15);
+	padding: 12px 0;
+	& > a {
+		text-decoration: none;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 4px 12px;
+		div {
+			display: flex;
+			flex-direction: column;
+			text-align: left;
+			span {
+				font-size: 12px;
+				line-height: 1.333;
+				&:first-child {
+					color: rgba(0, 0, 0, 0.6);
+				}
+				&:nth-child(2) {
+					color: #000;
+				}
+			}
+		}
+	}
+`;
+
+const Item = styled.a`
+	display: block;
+	border-color: rgba(0, 0, 0, 0.6);
+	text-align: left;
+	padding: 12px;
+	font-size: 12px;
+	span {
+		display: flex;
+		align-items: center;
+	}
+	&:hover {
+		background-color: rgba(0, 0, 0, 0.08);
+	}
+`;
+
+const CommunityCard = styled(ArtCard)`
+	padding: 8px 0 0;
+	text-align: left;
+	display: flex;
+	flex-direction: column;
+	a {
+		color: #000;
+		padding: 4px 12px;
+		font-size: 15px;
+		&:hover {
+			color: #A943D3;
+		}
+		span {
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+		}
+		&:last-child {
+			color: rgba(0, 0, 0, 0.6);
+			border-top: 1px solid #d6cec2;
+			padding: 12px;
+		}
+	}
+`;
+
+const Button = styled.button`
+  display: inline-block;
+  outline: 0;
+  text-align: left;
+  align-items: bottom;
+  background-color: white;
+  border-color: #fff;
+  border: 1px solid transparent;
+  color: black;
+  font-weight: 400;
+  border-radius: 30px;
+  font-size: 14px;
+  padding: 5px 10px;
+  margin: 10px 0px;
+  cursor: pointer;
+  text-align: center;
+  &:hover {
+    color: #A943D3;
+  }
+`;
+
+const LogoButton = styled.button`
+    display: inline-block;
+    outline: 0;
+    border: 0;
+    font-size: 16px;
+    font-weight: 400;
+    border: 2px solid #8f2bb8;
+    color: #000;
+    cursor: pointer;
+    border-radius: 100px;
+    padding: 10px 15px;
+    margin: 6px 0px;
+    white-space: nowrap;
+
+    :hover {
+        background-color: #F4ECF7;
+        background-image: none!important;
+    }
+`;
+
+const Title = styled.div`
+    font-size = 50px;
+    padding: 10px 15px;
+    font-weight: bold;
+    color: #8f2bb8;
+`;
+
+const sub = styled.div`
+    font-size = 25px:
+`;
+
+
+function Left(props) {
+	let user = props.user ? props.user : null;
+	let photoUrl = user ? user.photoURL : "/images/photo.svg";
+	let uid = props.user ? props.user.uid : null;
+	const [userType, setUserType] = useState("Renter");
+	const [info, changeBio] = useState("About You");
+
+	useEffect(()=>{
+		if (user){
+			setUserType(user.userInfo ? user.userInfo.status : "Renter");
+		} else {
+			setUserType("Renter");
+		}
+	},[user]);
+
+	const changeToLandlord = () => {
+		db.collection("profiles").doc(auth.currentUser.uid).update({
+			status: 'Landlord'
+		});
+		setUserType("Landlord");
+	}
+	
+	const changeToRenter = () => {
+		db.collection("profiles").doc(auth.currentUser.uid).update({
+			status: 'Renter'
+		});
+		setUserType("Renter");
+	}
+
+	const writeData = event => {
+		event.preventDefault();
+		var info = event.target.elements.bio.value;
+		changeBio(info);
+		db.collection("profiles").doc(auth.currentUser.uid).update({
+			bio: info
+		});
+
+	}
+
+	return (
+        <span>
+        <Header /> 
         <Sidebar />
-        <Head>
-            <div className="top">
-            
-            <Photo>{<img src={photoUrl} />}</Photo>
+		<Container>
+			<ArtCard>
+				<UserInfo>
+					<CardBackground />
+					<a>
+						<Photo photoUrl={photoUrl} />
+						<Link>{user ? user.displayName : "there"}</Link>
+					</a>
+					<h3>Status: {userType}</h3>
+					<a href={"/profile/"+uid}>
+						<AddPhotoText>
+							Edit Profile
+						</AddPhotoText>
+					</a>
+				</UserInfo>
+			</ArtCard>
+			<CommunityCard>
+                <Title> About Me</Title>
+					<a>
+							<h2>{info}</h2>
+							<form onSubmit={writeData}>
+								<input type="text" placeholder="Edit Bio" id="bio"/>
+								<LogoButton type="submit">Edit</LogoButton>
+							</form>
+						
+					</a>
 
-            </div>
+					<a>
+							<span>Change Your Status</span>
+                            <span>
+							<LogoButton onClick={changeToLandlord}>Landlord</LogoButton>
+                            </span>
+							<LogoButton onClick={changeToRenter}>Renter</LogoButton>
+						
+					</a>
+			
+            </CommunityCard>
+            <CommunityCard>
+                <Title> Experiences</Title>
+					<a>
+						
+							<span>Rented 217 Berry St. for 3.5 years</span>
+                            <span> - </span>
+							<span>Leased Your Mom for 15 years </span>
 
-        </Head>
-        <Grid>
-            <Div>
-
-            <span>{user ? user.displayName : null}</span>
-            
-            </Div>
-            
-        </Grid>
-        
-  
-
-    </div>);
+						
+					</a>
+			
+            </CommunityCard>
+			
+		</Container>
+        </span>
+	);
 }
 
 const mapStateToProps = (state) => {
 	return {
 		user: state.userState.user,
-        loggingIn: state.userState.loggingIn,
 	};
 };
 
-const mapDispatchToProps = (dispatch) => ({
-	getUserAuth: () => dispatch(getUserAuth()),
-});
-
-
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(Profile);
+export default connect(mapStateToProps)(Left);
