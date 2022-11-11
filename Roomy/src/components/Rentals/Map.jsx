3297fe730e4e56eb606dc2c919aef1ebd4c241a6
jalund <jalund@ucsc.edu>
2022-11-11 12:00:38 -0800

    start

--- Roomy/src/components/Rentals/Map.jsx
+++ Roomy/src/components/Rentals/Map.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { Component, useEffect, useState } from "react";
 import styled from "styled-components";
-import { GoogleMap, LoadScript, Marker} from "@react-google-maps/api";
+import { GoogleMap, LoadScript, Marker, InfoWindow } from "@react-google-maps/api";
 import { getRentalsAPI } from "../../action";
 import { connect } from "react-redux";
 
@@ -18,18 +18,70 @@ const center = {
     lng: -122.030792
 };
 
+const MyWindow = styled.div`
+    img {
+        width: 100px;
+        height: 100px;
+    }
+`;
+
 function Map(props) {
+    const [selectedPlace, setSelectedPlace] = useState(null);
+    const [rentalMap, setRentalMap] = useState({});
+    const [zoom, setZoom] = useState(14);
+    const [infoOpen, setInfoOpen] = useState(false);
+
+    const markerOnLoadHandler = (rental, key) => {
+        rental.setIcon("/images/rental-marker.png");
+        return setRentalMap(prevState => {
+          return { ...prevState, [key]: rental };
+        });
+    };
+
+    const markerOnClickHandler = (rental, key) => {
+        // This works because selectedPlace will only be set when infoOpen is true
+        // And checks infoOpen to make sure we can reopen it after closing.
+        // Goal: close on double click
+        if (infoOpen && selectedPlace[1] == key) {
+            setInfoOpen(false);
+            // console.log(selectedPlace[0]);
+            return;
+        }
+
+        setSelectedPlace([rental, key]);
+        if (infoOpen) {
+          setInfoOpen(false);
+        }
+
+        setInfoOpen(true);
+    };
+
     return (
-        <LoadScript googleMapsApiKey="AIzaSyC5k7rL8Ed7t1I-te1afRWN7B4DczsEJho">
-            <GoogleMap
-                mapContainerStyle={containerStyle}
-                center={center}
-                zoom={10}>
+        <LoadScript googleMapsApiKey="AIzaSyBX1z5nvjcjzyxSMT-QCVS3ERu6Y3iNSb0">
+            <GoogleMap 
+                mapContainerStyle={containerStyle} 
+                center={center} 
+                zoom={zoom}>
                     {props.rentals &&
                         props.rentals.map((rental, key)=> (
-                            <Marker key={key} position={{ lat: rental.coords.latitude, lng: rental.coords.longitude}} />
-                        ))
-                    }
+                            <Marker
+                                key={key}
+                                position={{ lat: rental.coords.latitude, lng: rental.coords.longitude }}
+                                onLoad={event => markerOnLoadHandler(event, key)}
+                                onClick={() => markerOnClickHandler(rental, key)}
+                                onMouseOver={() => {rentalMap[key].setIcon("/images/rental-marker-highlight.png");}}
+                                onMouseOut={() => {rentalMap[key].setIcon("/images/rental-marker.png");}}
+                            />
+                        ))}
+                        {infoOpen && selectedPlace && (
+                            <InfoWindow anchor={rentalMap[selectedPlace[1]]} onCloseClick={() => setInfoOpen(false)}>
+                                <MyWindow>
+                                    {selectedPlace[0].photos.length > 0 && <img src={selectedPlace[0].photos[0]} alt="" />}
+                                    <h1>{selectedPlace[0].title}</h1>
+                                    <div>{selectedPlace[0].description}</div>
+                                </MyWindow>
+                            </InfoWindow>
+                        )}
             </GoogleMap>
         </LoadScript>
     );
