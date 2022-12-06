686157a6c3d2a7145ca5a17446870bca4991ca43
Ravjodh Heer <ravjodhheer@Ravjodhs-MacBook-Pro.local>
2022-12-05 22:28:23 -0800

    minor UI tweaks

--- Roomy/src/components/Misc/ImageDisplay.jsx
+++ Roomy/src/components/Misc/ImageDisplay.jsx
@@ -2,34 +2,34 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-     
 `;
 
 const ButtonWrapper = styled.div`
     position: absolute;
-     width: 100%;
-     display: flex;
-     justify-content: space-between;
-     top: 50%;
-     left: 50%;
-     transform: translate(-50%, -50%);
-     button {
-         width: 45px;
-         height: 45px;
-         font-size: 15px;
-         border: none;
-         border-radius: 50px;
-         background-color: rgba(0, 0, 0, 0.15);
-         &:hover {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    button {
+        width: 45px;
+        height: 45px;
+        margin-top: 20px;
+        font-size: 15px;
+        border: none;
+        border-radius: 50px;
+        background-color: rgba(0, 0, 0, 0.15);
+        &:hover {
             color: white;
             background-color: rgba(49, 49, 49, 0.7);
         }
-     }
+    }
 `;
 
 const Indicator = styled.div`
      position: absolute;
-     bottom: 100px;
+     bottom: 60px;
      left: 50%;
      transform: translateX(-50%);
      padding: 10px 25px;
